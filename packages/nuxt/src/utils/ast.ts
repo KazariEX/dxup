c9ee7916281633ef4d9614429510b3fbf82127b1
KazariEX <zinkawaii@qq.com>
2025-09-26 02:18:04 +0800

    fix: cannot use `getChildren()`

--- packages/nuxt/src/utils/ast.ts
+++ packages/nuxt/src/utils/ast.ts
@@ -2,14 +2,22 @@ import type ts from "typescript";
 
 export function* forEachNode(node: ts.Node): Generator<ts.Node> {
     yield node;
-    for (const child of node.getChildren()) {
+    const children: ts.Node[] = [];
+    node.forEachChild((child) => {
+        children.push(child);
+    });
+    for (const child of children) {
         yield* forEachNode(child);
     }
 }
 
 export function walkNodes(node: ts.Node, callback: (node: ts.Node, next: () => void) => void) {
     function next() {
-        for (const child of node.getChildren()) {
+        const children: ts.Node[] = [];
+        node.forEachChild((child) => {
+            children.push(child);
+        });
+        for (const child of children) {
             walkNodes(child, callback!);
         }
     }
