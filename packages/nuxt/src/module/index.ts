f39b066fff09377aba8764cf9ee45ca6ae815113
KazariEX <zinkawaii@qq.com>
2025-09-25 17:58:26 +0800

    feat(nuxt): add `@dxup/unimport`

--- packages/nuxt/src/module/index.ts
+++ packages/nuxt/src/module/index.ts
@@ -7,13 +7,30 @@ export default defineNuxtModule({
         configKey: "dxup",
     },
     async setup(options, nuxt) {
-        nuxt.hook("prepare:types", (options) => {
-            ((options.tsConfig.compilerOptions ??= {}).plugins ??= []).push({
-                name: "@dxup/nuxt",
-            });
-            ((options.tsConfig.vueCompilerOptions ??= {}).plugins ??= []).push(
-                "@dxup/nuxt/vue/nitro-routes",
-            );
-        });
+        const pluginsTs = [
+            { name: "@dxup/nuxt" },
+            { name: "@dxup/unimport" },
+        ];
+
+        const pluginsVue = [
+            "@dxup/nuxt/vue/nitro-routes",
+        ]
+
+        append(pluginsTs, nuxt.options, "typescript", "tsConfig", "compilerOptions");
+        append(pluginsTs, nuxt.options, "typescript", "sharedTsConfig", "compilerOptions");
+        append(pluginsTs, nuxt.options.nitro, "typescript", "tsConfig", "compilerOptions");
+        append(pluginsVue, nuxt.options, "typescript", "tsConfig", "vueCompilerOptions");
     },
 });
+
+function append<
+    T extends Record<string, any>,
+    K0 extends keyof T,
+    K1 extends keyof NonNullable<T[K0]>,
+    K2 extends keyof NonNullable<NonNullable<T[K0]>[K1]>,
+>(plugins: any[], target: T, ...keys: [K0, K1?, K2?]) {
+    for (const key of keys) {
+        target = (target as any)[key] ??= {};
+    }
+    ((target as any).plugins ??= []).push(...plugins);
+}
