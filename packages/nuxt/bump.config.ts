ef9dde1a9315797648d609c0c9d62eb378c690fe
KazariEX <zinkawaii@qq.com>
2025-09-28 13:52:14 +0800

    chore: use shared script to bump version

--- packages/nuxt/bump.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "bumpp";
-import { name } from "./package.json";
-
-export default defineConfig({
-    push: false,
-    tag: `${name}@%s`,
-    commit: `release(${name.slice("@dxup/".length)}): v%s`,
-});
