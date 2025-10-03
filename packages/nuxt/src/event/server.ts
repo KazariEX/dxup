88676fcb3274b3d129612854ffa9f75118790d01
KazariEX <zinkawaii@qq.com>
2025-10-03 13:51:36 +0800

    refactor: do not ensure events file exists on server side

--- packages/nuxt/src/event/server.ts
+++ packages/nuxt/src/event/server.ts
@@ -1,4 +1,3 @@
-import { existsSync, writeFileSync } from "node:fs";
 import { appendFile } from "node:fs/promises";
 import { join } from "node:path";
 import type ts from "typescript";
@@ -6,12 +5,12 @@ import type { EventMap } from "./types";
 
 export function createEventServer(info: ts.server.PluginCreateInfo) {
     const path = join(info.project.getCurrentDirectory(), "dxup/events.md");
-    if (!existsSync(path)) {
-        writeFileSync(path, "");
-    }
 
     function write<K extends keyof EventMap>(key: K, data: EventMap[K][0]) {
-        return appendFile(path, `\`\`\`json {${key}}\n${JSON.stringify(data, null, 2)}\n\`\`\`\n`);
+        try {
+            return appendFile(path, `\`\`\`json {${key}}\n${JSON.stringify(data, null, 2)}\n\`\`\`\n`);
+        }
+        catch {}
     }
 
     return {
