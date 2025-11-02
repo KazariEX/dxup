10577eb9dc8ca74acd7ed78131638fbcf4250f56
KazariEX <zinkawaii@qq.com>
2025-11-03 01:47:15 +0900

    test: add references test logic

--- test/playground.test.ts
+++ test/playground.test.ts
@@ -8,8 +8,8 @@ import type { Language } from "@volar/language-core";
 
 describe("playground", () => {
     const logger: ts.server.Logger = {
-        close() {},
-        endGroup() {},
+        close: () => {},
+        endGroup: () => {},
         getLogFileName: () => void 0,
         hasLevel: () => false,
         info: () => {},
@@ -20,11 +20,11 @@ describe("playground", () => {
     };
 
     const session = new ts.server.Session({
-        byteLength: (buf, encoding) => Buffer.byteLength(buf, encoding),
+        byteLength: Buffer.byteLength,
         cancellationToken: ts.server.nullCancellationToken,
         canUseEvents: true,
         host: ts.sys as any,
-        hrtime: (start) => process.hrtime(start),
+        hrtime: process.hrtime,
         logger,
         useInferredProjectPerProjectRoot: false,
         useSingleInferredProject: false,
@@ -44,6 +44,7 @@ describe("playground", () => {
 
     const playgroundRoot = resolve(import.meta.dirname, "../playground");
     const appVuePath = resolve(playgroundRoot, "app/app.vue");
+    const buildDir = resolve(playgroundRoot, ".nuxt");
     projectService.openClientFile(appVuePath);
 
     const project = projectService.getDefaultProjectForFile(ts.server.toNormalizedPath(appVuePath), true)!;
@@ -51,79 +52,74 @@ describe("playground", () => {
     const program = languageService.getProgram()!;
     const language = (project as any).__vue__.language as Language<string>;
 
-    const sourceFile = program.getSourceFile(appVuePath)!;
-    const sourceScript = language.scripts.get(appVuePath);
-    const serviceScript = sourceScript?.generated!.languagePlugin.typescript?.getServiceScript(
-        sourceScript.generated!.root,
-    );
-    const sourceText = sourceScript?.snapshot.getText(0, sourceScript.snapshot.getLength()) ?? sourceFile.text;
+    const operationRE = /(?<=(?:\/\/|<!--)\s*)(?<range>\^—*\^)\((?<type>\w+)\)/;
 
-    function getSourceTextSpan(start: number, end: number) {
-        if (sourceScript && serviceScript) {
-            const map = language.maps.get(serviceScript.code, sourceScript);
-            // eslint-disable-next-line no-unreachable-loop
-            for (const range of map.toSourceRange(start - sourceText.length, end - sourceText.length, false)) {
-                return {
-                    start: range[0],
-                    length: range[1] - range[0],
-                };
+    for (const fileName of project.getFileNames()) {
+        if (fileName.startsWith(buildDir)) {
+            continue;
+        }
+
+        const sourceFile = program.getSourceFile(fileName)!;
+        const snapshot = language.scripts.get(fileName)?.snapshot;
+        const sourceText = snapshot?.getText(0, snapshot.getLength()) ?? sourceFile.text;
+
+        if (!operationRE.test(sourceText)) {
+            continue;
+        }
+
+        const lines = sourceText.split("\n");
+        const offsets = lines.reduce<number[]>((res, line, i) => {
+            res.push(i ? res.at(-1)! + lines[i - 1].length + 1 : 0);
+            return res;
+        }, []);
+
+        interface Item extends ts.TextSpan {
+            type: string;
+        }
+
+        const items: Item[] = [];
+        for (let i = 0; i < lines.length; i++) {
+            const line = lines[i];
+            const match = line.match(operationRE);
+            if (!match) {
+                continue;
             }
-        }
-        return { start, length: end - start };
-    }
 
-    const lines = sourceFile.text.split("\n");
-    const offsets = lines.reduce<number[]>((res, line, i) => {
-        res.push(i ? res.at(-1)! + lines[i - 1].length + 1 : 0);
-        return res;
-    }, []);
-
-    interface Item extends ts.TextSpan {
-        type: string;
-    }
-
-    const features: Record<string, Item[]> = {};
-    const featureRE = /\/\* -+ (?<name>.*) -+ \*\//;
-    const operationRE = /(?<=\/\/\s*)(?<range>\^—*\^)\((?<type>\w+)\)/;
-
-    let items: Item[] | undefined;
-    for (let i = 0; i < lines.length; i++) {
-        const line = lines[i];
-        const feature = line.match(featureRE);
-        if (feature) {
-            const { name } = feature.groups!;
-            features[name] = items = [];
-            continue;
+            const { range, type } = match.groups!;
+            items.push({
+                start: offsets[i - 1] + match.index!,
+                length: range.length,
+                type,
+            });
         }
 
-        const operation = line.match(operationRE);
-        if (!operation) {
-            continue;
-        }
-
-        const { range, type } = operation.groups!;
-        const offset = offsets[i - 1] + operation.index!;
-
-        items?.push({
-            ...getSourceTextSpan(offset, offset + range.length),
-            type,
-        });
-    }
-
-    for (const [name, items] of Object.entries(features)) {
-        it(name, () => {
+        describe(relative(playgroundRoot, fileName), () => {
             for (const { type, start, length } of items) {
-                if (type === "definition") {
-                    const result = languageService.getDefinitionAndBoundSpan?.(sourceFile.fileName, start);
-                    expect(result).toBeDefined();
-                    expect(result!.textSpan).toEqual({ start, length });
-                    expect(
-                        result!.definitions?.map((definition) => ({
-                            fileName: relative(playgroundRoot, definition.fileName),
-                            textSpan: definition.textSpan,
-                        })),
-                    ).toMatchSnapshot();
-                }
+                it(type, () => {
+                    if (type === "definition") {
+                        const result = languageService.getDefinitionAndBoundSpan?.(sourceFile.fileName, start);
+                        expect(result).toBeDefined();
+                        expect(result!.textSpan).toEqual({ start, length });
+                        expect(
+                            result!.definitions?.map((definition) => ({
+                                fileName: relative(playgroundRoot, definition.fileName),
+                                textSpan: definition.textSpan,
+                            })),
+                        ).toMatchSnapshot();
+                    }
+                    else if (type === "references") {
+                        const result = languageService.findReferences(sourceFile.fileName, start);
+                        expect(result).toBeDefined();
+                        expect(result!.length).toBe(1);
+                        expect(result![0].definition.textSpan).toEqual({ start, length });
+                        expect(
+                            result![0].references.slice(1).map((reference) => ({
+                                fileName: relative(playgroundRoot, reference.fileName),
+                                textSpan: reference.textSpan,
+                            })),
+                        ).toMatchSnapshot();
+                    }
+                });
             }
         });
     }
