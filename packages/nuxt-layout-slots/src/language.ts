74ac7c0f8813020eb9cdaaec0a54add9a0e59c80
KazariEX <zinkawaii@qq.com>
2026-05-11 04:02:22 +0800

    refactor: simplify

--- packages/nuxt-layout-slots/src/language.ts
+++ packages/nuxt-layout-slots/src/language.ts
@@ -11,110 +11,93 @@ interface Config {
 const plugin: VueLanguagePlugin<Config> = ({
     modules: { typescript: ts, "@vue/compiler-dom": CompilerDOM },
     config: { options },
-}) => {
-    const variableName = "__VLS_layout";
+}) => ({
+    version: 2.2,
+    order: -1,
+    resolveEmbeddedCode(fileName, sfc, embeddedCode) {
+        if (!embeddedCode.id.startsWith("script_")) {
+            return;
+        }
 
-    return [{
-        version: 2.2,
-        order: -1,
-        resolveEmbeddedCode(fileName, sfc, embeddedCode) {
-            if (!embeddedCode.id.startsWith("script_")) {
+        if (!options.dirs.some((dir) => fileName.startsWith(dir))) {
+            return;
+        }
+
+        if (!sfc.template?.ast) {
+            return;
+        }
+
+        if (sfc.template.ast.children.length === 1) {
+            const root = sfc.template.ast.children[0];
+            if (root.loc.start.offset === Number.MAX_VALUE) {
                 return;
             }
+        }
 
-            if (!options.dirs.some((dir) => fileName.startsWith(dir))) {
-                return;
-            }
+        let layoutName = "default";
+        if (sfc.scriptSetup) {
+            visit(sfc.scriptSetup.ast);
+        }
 
-            if (!sfc.template?.ast) {
-                return;
-            }
-
-            if (sfc.template.ast.children.length === 1) {
-                const root = sfc.template.ast.children[0];
-                if (root.loc.start.offset === Number.MAX_VALUE) {
-                    return;
-                }
-            }
-
-            const children = sfc.template.ast.children;
-            sfc.template.ast.children = [{
-                type: CompilerDOM.NodeTypes.ELEMENT,
-                ns: CompilerDOM.Namespaces.HTML,
-                tag: "component",
-                tagType: CompilerDOM.ElementTypes.COMPONENT,
-                loc: createVirtualLoc(),
-                props: [{
-                    type: CompilerDOM.NodeTypes.DIRECTIVE,
-                    name: "bind",
-                    arg: {
-                        type: CompilerDOM.NodeTypes.SIMPLE_EXPRESSION,
-                        content: "is",
-                        isStatic: true,
-                        constType: CompilerDOM.ConstantTypes.CAN_STRINGIFY,
-                        loc: createVirtualLoc("is"),
-                    },
-                    exp: {
-                        type: CompilerDOM.NodeTypes.SIMPLE_EXPRESSION,
-                        content: variableName,
-                        isStatic: false,
-                        constType: CompilerDOM.ConstantTypes.NOT_CONSTANT,
-                        loc: createVirtualLoc(variableName),
-                    },
-                    modifiers: [],
-                    loc: createVirtualLoc(`:is="${variableName}"`),
-                }],
-                children,
-                codegenNode: void 0,
-            }];
-        },
-    }, {
-        version: 2.2,
-        resolveEmbeddedCode(fileName, sfc, embeddedCode) {
-            if (!embeddedCode.id.startsWith("script_")) {
-                return;
-            }
-
-            if (!options.dirs.some((dir) => fileName.startsWith(dir))) {
-                return;
-            }
-
-            let layoutName = "default";
-            if (sfc.scriptSetup) {
-                visit(sfc.scriptSetup.ast);
-            }
-
-            function visit(node: ts.Node) {
-                if (
-                    ts.isCallExpression(node) &&
-                    ts.isIdentifier(node.expression) &&
-                    node.expression.text === "definePageMeta" &&
-                    node.arguments.length &&
-                    ts.isObjectLiteralExpression(node.arguments[0])
-                ) {
-                    for (const prop of node.arguments[0].properties) {
-                        if (
-                            ts.isPropertyAssignment(prop) &&
-                            ts.isIdentifier(prop.name) &&
-                            prop.name.text === "layout" &&
-                            ts.isStringLiteral(prop.initializer)
-                        ) {
-                            layoutName = prop.initializer.text;
-                            break;
-                        }
+        function visit(node: ts.Node) {
+            if (
+                ts.isCallExpression(node) &&
+                ts.isIdentifier(node.expression) &&
+                node.expression.text === "definePageMeta" &&
+                node.arguments.length &&
+                ts.isObjectLiteralExpression(node.arguments[0])
+            ) {
+                for (const prop of node.arguments[0].properties) {
+                    if (
+                        ts.isPropertyAssignment(prop) &&
+                        ts.isIdentifier(prop.name) &&
+                        prop.name.text === "layout" &&
+                        ts.isStringLiteral(prop.initializer)
+                    ) {
+                        layoutName = prop.initializer.text;
+                        break;
                     }
                 }
-                else {
-                    ts.forEachChild(node, visit);
-                }
             }
+            else {
+                ts.forEachChild(node, visit);
+            }
+        }
 
-            embeddedCode.content.push(
-                `declare const ${variableName}: import("#build/dxup/layouts").Layouts["${layoutName}"];\n`,
-            );
-        },
-    }];
-};
+        const expression = `\n// @ts-ignore\n{} as import("#build/dxup/layouts").Layouts["${layoutName}"]\n`;
+        const children = sfc.template.ast.children;
+
+        sfc.template.ast.children = [{
+            type: CompilerDOM.NodeTypes.ELEMENT,
+            ns: CompilerDOM.Namespaces.HTML,
+            tag: "component",
+            tagType: CompilerDOM.ElementTypes.COMPONENT,
+            loc: createVirtualLoc(),
+            props: [{
+                type: CompilerDOM.NodeTypes.DIRECTIVE,
+                name: "bind",
+                arg: {
+                    type: CompilerDOM.NodeTypes.SIMPLE_EXPRESSION,
+                    content: "is",
+                    isStatic: true,
+                    constType: CompilerDOM.ConstantTypes.CAN_STRINGIFY,
+                    loc: createVirtualLoc("is"),
+                },
+                exp: {
+                    type: CompilerDOM.NodeTypes.SIMPLE_EXPRESSION,
+                    content: expression,
+                    isStatic: false,
+                    constType: CompilerDOM.ConstantTypes.NOT_CONSTANT,
+                    loc: createVirtualLoc(expression),
+                },
+                modifiers: [],
+                loc: createVirtualLoc(`:is="${expression}"`),
+            }],
+            children,
+            codegenNode: void 0,
+        }];
+    },
+});
 
 export default plugin;
 
