b87c12e52ea9228b369a0d4c5833bc805d3c6e8c
KazariEX <zinkawaii@qq.com>
2026-06-20 14:10:29 +0800

    refactor: replace `zx` with `tinyexec`

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,9 +1,12 @@
 import { resolve } from "pathe";
-import { $ } from "zx";
+import { exec } from "tinyexec";
 
 const tag = process.env.GITHUB_REF_NAME!;
 const packageName = tag.split("@")[1].slice("dxup/".length);
 
-await $({
-  cwd: resolve(import.meta.dirname, "../packages", packageName),
-})`pnpm publish --access public --no-git-checks`;
+await exec("pnpm", ["publish", "--access", "public", "--no-git-checks"], {
+  nodeOptions: {
+    cwd: resolve(import.meta.dirname, "../packages", packageName),
+  },
+  throwOnError: true,
+});
